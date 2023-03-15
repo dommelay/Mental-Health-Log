f415161f926a29de102839ab6fb4be9b18813491
Dominique Clark <melaydominique@gmail.com>
2023-03-15 12:14:57 -0400

    update

--- public/script.js
+++ public/script.js
@@ -1,11 +1,39 @@
 $(() => {
 $('#sad').on('click', () => {
-    $('#feeling').attr('value', 'sad')
+    $('#feeling').attr('value', 'Sad')
     $('#feelingemoji').attr('value', 'https://cdn.shopify.com/s/files/1/1061/1924/products/Sleeping_with_Snoring_Emoji_large.png?v=1571606036')
 })
 $('#happy').on('click', () => {
-    $('#feeling').attr('value', 'happy')
+    $('#feeling').attr('value', 'Happy')
     $('#feelingemoji').attr('value', 'https://cdn.shopify.com/s/files/1/1061/1924/products/Smiling_Emoji_with_Smiling_Eyes_large.png?v=1571606035')
+})
+$('#annoyed').on('click', () => {
+    $('#feeling').attr('value', 'Annoyed')
+    $('#feelingemoji').attr('value', 'https://cdn.shopify.com/s/files/1/1061/1924/products/Unamused_Face_Emoji_761d8bf8-c78c-45b1-80b1-a86a80d2452d_large.png?v=1571606036')
+})
+$('#ashamed').on('click', () => {
+    $('#feeling').attr('value', 'Ashamed')
+    $('#feelingemoji').attr('value', 'https://cdn.shopify.com/s/files/1/1061/1924/products/Persevering_Face_Emoji_large.png?v=1571606037')
+})
+$('#proud').on('click', () => {
+    $('#feeling').attr('value', 'Proud')
+    $('#feelingemoji').attr('value', 'https://cdn.shopify.com/s/files/1/1061/1924/products/Relieved_Emoji_large.png?v=1571606037')
+})
+$('#stressed').on('click', () => {
+    $('#feeling').attr('value', 'Stressed')
+    $('#feelingemoji').attr('value', 'https://cdn.shopify.com/s/files/1/1061/1924/products/Face_with_Cold_Sweat_Emoji_large.png?v=1571606037')
+})
+$('#angry').on('click', () => {
+    $('#feeling').attr('value', 'Angry')
+    $('#feelingemoji').attr('value', 'https://cdn.shopify.com/s/files/1/1061/1924/products/Angry_Emoji_large.png?v=1571606038')
+})
+$('#worried').on('click', () => {
+    $('#feeling').attr('value', 'Worried')
+    $('#feelingemoji').attr('value', 'https://cdn.shopify.com/s/files/1/1061/1924/products/Worried_Face_Emoji_12fa1bbf-0297-4818-8298-d6c1a588c341_large.png?v=1571606036')
+})
+$('#hopeful').on('click', () => {
+    $('#feeling').attr('value', 'Hopeful')
+    $('#feelingemoji').attr('value', 'https://cdn.shopify.com/s/files/1/1061/1924/products/Shyly_Smiling_Face_Emoji_large.png?v=1571606037')
 })
 
 
